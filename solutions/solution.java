b3709e2c793effe9cbde398be47fc4ab4dcd69bf
Vishwanath-Akshay <mail2akshay14@gmail.com>
2025-04-03 15:41:39 +0530

    Test.java file testcases added

--- solutions/solution.java
+++ solutions/solution.java
@@ -1,5 +1,14 @@
-class Solution {
+public class solution {
     public int missingNumber(int[] nums) {
+        int actualSum = 0, totalSum = 0;
+        int n = nums.length;
         
+        for (int num : nums) {
+            actualSum += num;
+        }
+        
+        totalSum = (n * (n + 1)) / 2;
+        
+        return totalSum - actualSum;
     }
 }

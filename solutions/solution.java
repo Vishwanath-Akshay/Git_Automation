193cadbbb239608150d813ce2a0eda006bc6b1f1
Vishwanath-Akshay <mail2akshay14@gmail.com>
2025-04-04 10:51:37 +0530

    updated java

--- solutions/solution.java
+++ solutions/solution.java
@@ -3,7 +3,7 @@ public class Solution {
     public int missingNumber(int[] nums) {
         int actualSum = 0, totalSum = 0;
         int n = nums.length;
-
+ 
         for (int num : nums) {
             actualSum += num;
         }
@@ -13,4 +13,4 @@ public class Solution {
         return totalSum - actualSum;
     }
 }
-//end
+//endd
